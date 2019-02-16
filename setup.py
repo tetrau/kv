43c45108849ee6f1038859202005c9d96016c97c
tetrau <tetrau01@gmail.com>
2019-02-16 23:19:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='kv',
+    version='0.0.1',
+    packages=['kv'],
+    url='https://github.com/tetrau/kv',
+    license='GPLv3',
+    author='tetrau',
+    python_requires='>=3.5.0',
+    author_email='tetrau01@gmail.com',
+    description='Python key-value storage over multiple types of databases.'
+)
